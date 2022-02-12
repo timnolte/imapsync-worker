85e7e70ce4167cf06f79b705e0207ed69b395435
Tim Nolte <tim.nolte@ndigitals.com>
2022-02-11 23:13:39 -0500

    Adjusts Dockerfile & script setup to allow for additional runtime flags.

--- emailsync.sh
+++ emailsync.sh
@@ -2,7 +2,7 @@
 
 case $1 in
   "gmail2gmail")
-    imapsync --user1 $USER1 --password1 $PASSWORD1 --user2 $USER2 --password2 $PASSWORD2 --subfolder2 $USER1 --gmail1 --gmail2 --dry
+    imapsync --user1 $USER1 --password1 $PASSWORD1 --user2 $USER2 --password2 $PASSWORD2 --subfolder2 $USER1 --gmail1 --gmail2 $OPTS
   ;;
   *)
     echo "Usage: sync gmail2gmail"
